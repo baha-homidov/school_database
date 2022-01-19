ae99574cef6dce068b8ab056e12cd2e6c2f6ebd5
Baha Hoidov <khamidov.mirzo@gmail.com>
2022-01-19 22:28:51 +0800

    git

---
+++ student.h
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Student{
+public:
+    Student(string new_name, int new_number, int new_birth_date, string new_major);
+    void set_name(string name);
+    void set_id_number(int number);  
+    void set_birth_date(int birth_date);
+    void set_major(string major);
+  
+    string name;
+    int id_number;
+    int birth_date;
+    string major;
+
+};
+//************************************************************************************************
+
+Student::Student(string new_name, int new_number, int new_birth_date, string new_major){
+    name=new_name;
+    id_number=new_number;
+    birth_date=new_birth_date;
+    major=new_major;
+};
+
