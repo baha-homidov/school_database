16a04630e7bc99f3bce169df9d255823cc0bcba3
Baha Hoidov <khamidov.mirzo@gmail.com>
2022-01-19 22:16:39 +0800

    cleaned up the directory

--- main.cpp
+++ main.cpp
@@ -2,13 +2,33 @@
 #include <algorithm>
 using namespace std;
 
+class Student{
+public:
+    Student(string new_name, int new_number, int new_birth_date, string new_major);
+    void set_name(string name);
+    void set_id_number(int number);  
+    void set_birth_date(int birth_date);
+    void set_major(string major);
+  
+    string name;
+    int id_number;
+    int birth_date;
+    string major;
 
+};
+//************************************************************************************************
 
+Student::Student(string new_name, int new_number, int new_birth_date, string new_major){
+    name=new_name;
+    id_number=new_number;
+    birth_date=new_birth_date;
+    major=new_major;
+};
 
 
 int main(){
-   while(true)
-    cout<<"howdy\n";
+  Student stud("Abdukadu Olimov", 2001, 20010602, "computer science");
+  
 }
 
 
